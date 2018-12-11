2e5387504912dd7aaab7341957623dae43aa1a66
Michael Kim <m.kim4247@gmail.com>
2018-12-11 15:41:20 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+class EyesOnMe extends Component {
+  render(){
+    return (
+      <button
+      onFocus={this.focusEvent}
+      onBlur={this.blurEvent} />
+    )
+  }
+
+  focusEvent = () => {
+    console.log('Good!')
+  }
+
+  blurEvent = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+}
+
+
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React, {Component} from 'react'
+
+class Keypad extends Component {
+
+  render(){
+    return(
+      <input
+      type="password"
+      onKeyUp={this.printOut} />
+    )
+  }
+
+  printOut = () => {
+    console.log('Entering password...')
+  }
+
+}
+
+export default Keypad
